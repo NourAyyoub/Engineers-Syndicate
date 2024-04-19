6a6a5b7fd26496155b62c694a2149be74bbd6af9
Nour Ayyoub <nour.ayyoub360@gmail.com>
2024-04-19 17:28:13 +0200

    make some changes on code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,16 @@
 import { Container } from "react-bootstrap";
-//import ProjectsTable from "./components/ProjectsTable/ProjectsTable";
+import ProjectsTable from "./components/ProjectsTable/ProjectsTable";
 // import AddNewProject from "./components/AddNewProject/AddNewProject";
 // import ProjectInfo from "./components/ProjectInfo/ProjectInfo";
-import AddNewVisit from "./components/AddNewVisit/AddNewVisit";
+// import AddNewVisit from "./components/AddNewVisit/AddNewVisit";
 
 export default function App() {
   return (
     <Container dir="rtl" className="text-center">
-      {/* <ProjectsTable /> */}
+      <ProjectsTable />
       {/* <ProjectInfo /> */}
       {/* <AddNewProject /> */}
-      <AddNewVisit />
+      {/* <AddNewVisit /> */}
     </Container>
   );
 }
